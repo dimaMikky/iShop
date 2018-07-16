9e5080ca90b61cd3a991a9f813aaa6d5174ee9d6
dimaMikky <mozgmikky@gmail.com>
2018-07-16 22:45:46 +0300

    add search and pagination

---
+++ app/controllers/SearchController.php
@@ -0,0 +1,33 @@
+<?php 
+
+namespace app\controllers;
+
+class SearchController extends AppController {
+
+    public function indexAction() {
+
+        $query = !empty(trim($_GET['s'])) ?  $_GET['s'] : null;
+        if($query) {
+            $products = \R::find('product', "title LIKE ?",["%{$query}%"]);
+        }
+        $this->setMeta('Поиск по: ' . h($query));
+        $this->set(compact('products', 'query'));
+
+    } 
+
+    public function typeaheadAction() {
+        if($this->isAjax()) {
+           $query = !empty(trim($_GET['query'])) ?  $_GET['query'] : null;
+           if($query) {
+               $products = \R::getAll('SELECT id, title 
+                        FROM product 
+                        WHERE title 
+                        LIKE ? LIMIT 10', 
+                        ["%{$query}%"]);
+                echo json_encode($products);
+           }
+        }
+        die;
+    }
+
+}
